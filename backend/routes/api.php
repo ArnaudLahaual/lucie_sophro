081650f638ff180141779af6291e02d111d73ff7
ArnaudLahaual <arnaud.lahaual@gmail.com>
2026-04-30 18:40:39 +0200

    creation userModel, authcontroller et routes pour le backOffice, function login et logout du User

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\TimeSlotController;
@@ -11,3 +12,12 @@ Route::get('/slots', [TimeSlotController::class, 'index']);
 //post
 Route::post('/contact', [ContactController::class, 'store']);
 Route::post('/addBooking', [BookingController::class, 'store']);
+
+//auth
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+// Routes admin protégées
+Route::middleware('auth:sanctum')->group(function () {
+    //  futures routes back office
+});
